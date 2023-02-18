b0342734435b80b556ea5f61c192bfd146549093
Regis-Oliveira <regis_crysis@hotmail.com>
2023-02-18 12:11:12 -0300

    fix: minutes amount input not show value

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -33,7 +33,7 @@ export function NewCycleForm() {
         min={5}
         max={60}
         disabled={!!activeCycle}
-        {...(register('minutesAmount'), { valueAsNumber: true })}
+        {...register('minutesAmount', { valueAsNumber: true })}
       />
 
       <span>minutes.</span>
